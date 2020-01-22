35a677006e26dc824d5dd4fc6c0a1fe3be918462
haterofdietcoke <56701478+haterofdietcoke@users.noreply.github.com>
2020-01-22 15:38:47 -0500

    Made boat hard to avoid, eliminated unused script

--- game/assets/scripts/boat_move.js
+++ game/assets/scripts/boat_move.js
@@ -11,7 +11,7 @@ cc.Class({
 
     start () {
         this.node.setPosition(0, 500);
-        var moveTo = cc.moveTo(5, 1099, 500);
+        var moveTo = cc.moveTo(2, 1100, 500);
         var time = cc.delayTime(Math.random() * 5);
         var moveBack = cc.moveTo(0, 0, 500);
         var seq = cc.sequence(moveTo, time, moveBack);
