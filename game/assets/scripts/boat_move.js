43ac7b7d2763a9b86fc2424984e1df104f5958c6
haterofdietcoke <56701478+haterofdietcoke@users.noreply.github.com>
2020-01-22 15:51:11 -0500

    Made sure that delay time would never be 0

--- game/assets/scripts/boat_move.js
+++ game/assets/scripts/boat_move.js
@@ -12,7 +12,7 @@ cc.Class({
     start () {
         this.node.setPosition(0, 500);
         var moveTo = cc.moveTo(2, 1100, 500);
-        var time = cc.delayTime(Math.random() * 5);
+        var time = cc.delayTime(Math.random() * 5 + 2);
         var moveBack = cc.moveTo(0, 0, 500);
         var seq = cc.sequence(moveTo, time, moveBack);
         var action = cc.repeatForever(seq);
