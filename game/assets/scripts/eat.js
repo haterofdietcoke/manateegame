6c86f2050619073c617b6d57f103f306ad53ac6b
Jan Fic <43736433+janfic@users.noreply.github.com>
2020-01-23 15:14:47 -0500

    added food drain

---
+++ game/assets/scripts/eat.js
@@ -0,0 +1,34 @@
+// Learn cc.Class:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        energy_bar: {
+            default: null,
+            type: cc.ProgressBar
+        },
+        rate: 0.05,
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+
+    start () {
+
+    },
+
+    update(dt) {
+    
+        this.energy_bar.progress -= this.rate * dt;
+    },
+});
