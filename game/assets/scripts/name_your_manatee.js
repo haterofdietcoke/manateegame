3360b6cbeba0e0d11626e2b8de7b8b11433001bd
haterofdietcoke <56701478+haterofdietcoke@users.noreply.github.com>
2020-02-19 07:18:44 -0500

    created new low water sprite, deleted start screen

--- game/assets/scripts/name_your_manatee.js
+++ game/assets/scripts/name_your_manatee.js
@@ -12,22 +12,28 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        sand: {
-            default: null,
-            type: cc.Node
-        }
-        water: {
-            default: null,
-            type: cc.Node
-        }
+        // foo: {
+        //     // ATTRIBUTES:
+        //     default: null,        // The default value will be used only when the component attaching
+        //                           // to a node for the first time
+        //     type: cc.SpriteFrame, // optional, default is typeof default
+        //     serializable: true,   // optional, default is true
+        // },
+        // bar: {
+        //     get () {
+        //         return this._bar;
+        //     },
+        //     set (value) {
+        //         this._bar = value;
+        //     }
+        // },
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {},
 
-    start() {
-        x_value = cc.Node.x;
+    start () {
 
     },
 
